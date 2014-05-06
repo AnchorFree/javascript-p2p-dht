7da1add8756777ed88b4c631f3571447d199db55
Anton Vishnyak <antonv@anchorfree.com>
2014-05-05 19:46:16 -0700

    Removed extra un-needed libraries.

--- Peer.js
+++ Peer.js
@@ -1,7 +1,7 @@
     var myId = uuid.v4(), myConn;
     var connectedPeers = {};
     var dataCache = {};
-    
+
     var peer = new Peer(null, {
         key: 'gm6cty1w0ptrcnmi',
 
@@ -13,48 +13,48 @@
             var copy = Array.prototype.slice.call(arguments).join(' ');
             $('#log').append(copy + '<br>');
         },
-        
+
         // Use a TURN server for more network support
         config: {'iceServers': [
             { url: 'stun:stun.l.google.com:19302' }
         ]} /* Sample servers, please use appropriate ones */
     });
-    
+
     var peerMap = new PeerMap(myId);
     var tracker = peer.connect('TRACKER', { metadata: { id: myId }});
-    
+
     peer.on('open', function(id) {
         Util.log('Connected established as ' + id + ' with id ' + myId);
-        
+
         myConn = id;
     });
-    
+
     peer.on('connection', handlePeer);
-    
+
     function handlePeer(conn) {
         Util.log('Connection from : ' + conn.peer);
-        
+
         if (conn.label === 'peer') {
             connectedPeers[conn.peer] = conn;
-            
+
             conn.on('data', function (data) {
                 Util.log('Data from : ' + conn.peer + ' : ' + JSON.stringify(data));
-                
+
                 if (typeof data === 'object') {
                     if (data.hasOwnProperty('q')) {
                         if (data.q === 'ping') {
                             peerMap.peerFound(data.id, undefined, conn.peer);
-                                         
+
                             conn.send({q: 'pong', id: myId});
                         } else if (data.q === 'pong') {
                             peerMap.peerFound(data.id, undefined, conn.peer);
-                            
+
                             // Peer verified... announce it to my other peers (except itself)
                             for (var property in peerMap.verifiedPeers) {
                                 if (peerMap.verifiedPeers.hasOwnProperty(property)) {
                                     if (typeof connectedPeers[peerMap.verifiedPeers[nearest].peerId] === 'undefined') {
                                         peerMap.peerFailed(peerMap.verifiedPeers[nearest].peerId, 'ProbablyOffline');
-                                        
+
                                         delete connectedPeers[peerMap.verifiedPeers[nearest].peerId];
                                     } else {
                                         connectedPeers[peerMap.verifiedPeers[property].peerId].send({q: 'announce_peer', id: data.id, conn: conn.peer });
@@ -65,17 +65,17 @@
                             // Do I have this key?
                             if (dataCache.hasOwnProperty(data.id)) {
                                 Util.log('Data found for key ' + data.id);
-                                
+
                                 conn.send({q: 'peer_found', data: dataCache[data.id]});
                             } else {
                                 // Do I know of a peer that is closer to this key?
                                 var neighbor = peerMap.nearestPeer(data.id);
-                            
+
                                 Util.log('Redirecting to peer ' + neighbor[1]);
                                 conn.send({q: 'peer_found', id: data.id, redirect: neighbor[1]});
                             }
                         } else if (data.q === 'peer_found') {
-                            if (data.hasOwnProperty('redirect')) {                            
+                            if (data.hasOwnProperty('redirect')) {
                                 connectToPeer(data.redirect, {q: 'find_peer', id: data.id });
                             } else if (data.hasOwnProperty('data')) {
                                 // Data found!
@@ -86,14 +86,14 @@
                         } else if (data.q === 'announce_peer') {
                             // Do I have any keys that are closer to the new peer?
                             peerMap.peerFound(data.id, conn.peer, conn.peer);
-                            
+
                             if (Object.keys(connectedPeers).length < 3) {
                                 // Connect to the peer
                                 connectToPeer(data.conn, {q: 'ping', id: myId});
                             }
                         } else if (data.q === 'store') {
                             put(data.id, data.value);
-                            
+
                             conn.send({q: 'store_verified', id: data.id });
                         } else if (data.q === 'store_verified') {
                             delete dataCache[data.id];
@@ -101,18 +101,18 @@
                     }
                 }
             });
-            
+
             conn.on('close', function () {
                 Util.log('Connection closed with ' + conn.peer);
-                
+
                 //peerMap.peerFailed(conn.peer, )
-                
+
                 delete connectedPeers[conn.peer];
             });
-            
+
             conn.on('error', function (err) {
-                
-            })
+                // TODO: Add error handling here
+            });
         }
     }
 
@@ -125,7 +125,7 @@
     tracker.on('data', function(data) {
         Util.log('Received bootstrap. ' + data);
 
-        if (data instanceof Array) {        
+        if (data instanceof Array) {
             data.forEach(function(element) {
                 connectToPeer(element, {q: 'ping', id: myId});
             });
@@ -136,35 +136,35 @@
                 tracker.send('bootstrap');
             }, 10000);
         }
-        
+
         setInterval(function() {
             if (connectedPeers.length === 0) {
                 tracker.send('bootstrap');
             }
         }, 10000);
-    })
+    });
 
     window.onunload = window.onbeforeunload = function(e) {
         if (!! peer && !peer.destroyed) {
             peer.destroy();
         }
     };
-    
+
     function connectToPeer(peerConnId, onConnect) {
         var tmp;
-        
+
         // TODO: Check peer statistics before connecting
         if (myConn !== peerConnId) {
             Util.log('Connecting to peer : ' + peerConnId);
-            
+
             tmp = peer.connect(peerConnId, {
                 label: 'peer',
                 metadata: { id: myId }
             });
-            
+
             tmp.on('open', function () {
                 handlePeer(tmp);
-                
+
                 if (typeof onConnect === 'function') {
                     onConnect(tmp);
                 } else if (typeof onConnect === 'object') {
@@ -173,49 +173,49 @@
             });
         }
     }
-    
+
     function put(key, value) {
         dataCache[key] = value;
     }
-    
+
     function get(key) {
         // Do we have it locally?
         if (dataCache.hasOwnProperty(key)) {
             return dataCache[key];
         }
-        
+
         // Do I know of a peer that is closer to this key?
         var neighbor = peerMap.nearestPeer(key);
-        
+
         connectedPeers[neighbor[1]].send({q: 'find_peer', id: key });
     }
-    
+
     function maintenance() {
         var myParsedId = uuid.parse(myId), nearest;
-        
+
         if (Object.keys(connectedPeers).length < 4) {
             Util.log('Rebalancing failed. No enough peers.  Attempting to bootstrap again.');
-            
+
             tracker.send('bootstrap');
         }
-        
+
         Util.log('Rebalancing.');
-        
-        for (var key in dataCache) {        
+
+        for (var key in dataCache) {
             // Find peer that is closest to the key in question
-            
+
             var x = Object.keys(peerMap.verifiedPeers).map(function (x) {
                 return [x, peerMap.config.distanceCalc(uuid.parse(x), uuid.parse(key))]
             });
-            
+
             // Add ourself as a peer in case we are the closest
             x.unshift([myId, peerMap.config.distanceCalc(myParsedId, uuid.parse(key))]);
-            
+
             nearest = x.sort(function (a, b) { return a[1]>b[1] })[0][0];
-                        
+
             if (nearest !== myId) {
                 Util.log('Sending key ' + key + ' to ' + peerMap.verifiedPeers[nearest].peerId);
-                
+
                 if (typeof connectedPeers[peerMap.verifiedPeers[nearest].peerId] === 'undefined') {
                     delete connectedPeers[peerMap.verifiedPeers[nearest].peerId];
                 } else {
@@ -224,33 +224,33 @@
             }
         }
     }
-    
+
     setInterval(maintenance, 10000);
-    
+
     setInterval(function () {
         var container = $('#data tbody'), cache = [];
-        
+
         container.empty();
-        
+
         for (var key in dataCache) {
             if (dataCache.hasOwnProperty(key)) {
-                cache.push('<tr><td>' + key + '</td><td>' + dataCache[key] + '</td></tr>')
+                cache.push('<tr><td>' + key + '</td><td>' + dataCache[key] + '</td></tr>');
             }
         }
-        
+
         container.append(cache);
     }, 1000);
-    
+
     setInterval(function () {
         var container = $('#peers tbody'), cache = [];
-        
+
         container.empty();
-        
+
         for (var key in peerMap.verifiedPeers) {
             if (peerMap.verifiedPeers.hasOwnProperty(key)) {
-                cache.push('<tr><td>' + peerMap.verifiedPeers[key].peerId + '</td><td>' + key + '</td></tr>')
+                cache.push('<tr><td>' + peerMap.verifiedPeers[key].peerId + '</td><td>' + key + '</td></tr>');
             }
         }
-            
+
         container.append(cache);
     }, 1000);
--- README.md
+++ README.md
@@ -3,8 +3,18 @@ javascript-p2p
 
 Experiment for building a JavaScript-based DHT for storage.
 
+Instructions
+------------
+1. Launch tracker.htm in a browser (Chrome 32+ or FF 29+)
+1. Launch one or more copies of peer.htm in a browser (Chrome 32+ or FF 29+)
+
 Documentation
 -------------
+
+P2P Framework
 * [PeerStatistics](http://www.explainjs.com/explain?src=https://raw.githubusercontent.com/AnchorFree/javascript-p2p-dht/master/p2p/PeerStatistics.js)
 * [PeerMap](http://www.explainjs.com/explain?src=https://raw.githubusercontent.com/AnchorFree/javascript-p2p-dht/master/p2p/PeerMap.js)
 * [Util](http://www.explainjs.com/explain?src=https://raw.githubusercontent.com/AnchorFree/javascript-p2p-dht/master/p2p/Util.js)
+
+DHT Framework
+* TBD
